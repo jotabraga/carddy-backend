e7a5fae8b268ebe26dfaf6f5cbd838933b954b55
luanerdy <luan.vini1@gmail.com>
2021-08-28 14:52:41 -0300

    update: database synchro

--- src/database.ts
+++ src/database.ts
@@ -7,6 +7,7 @@ export default async function connect() {
         type: "postgres",
         url: process.env.DATABASE_URL,
         entities: [`${process.env.NODE_ENV === 'production' ? 'dist' : 'src'}/entities/*.*`],
+        synchronize: true,
         extra: {
             ssl: true
         }
