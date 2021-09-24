1a7114fae011f68c292676525c375b89edcfcfc9
jotabraga <jpmbraga.feg@gmail.com>
2021-09-23 23:16:13 -0300

    feat: add jwt to authenticate middleware

--- src/middlewares/authenticate.ts
+++ src/middlewares/authenticate.ts
@@ -1,25 +1,33 @@
 import { Request, Response, NextFunction } from "express";
-import { getRepository } from "typeorm";
-import Session from "../entities/Session";
+import jwt from "jsonwebtoken";
 
+import * as sessionService from "@/services/client/session";
+import UnauthorizedError from "@/errors/Unauthorized";
 
-export default async function middlewareAuth(req: Request, res: Response, next: NextFunction){
-    try{
-        const authorization = req.headers["authorization"];
+interface JwtPayload {
+    userId: number
+}
 
-        if(!authorization) return res.sendStatus(401);
+export default async function authenticationMiddleware(req: Request, res: Response, next: NextFunction) {
+  try {
+    const authHeader = req.header("Authorization");
 
-        const token = authorization.split("Bearer ")[1];
-        if(!token) return res.sendStatus(401);
+    const token = authHeader?.replace("Bearer ", "");
+    if (!token) {
+      throw new UnauthorizedError();
+    } 
+  
+    const { userId } = jwt.verify(token, process.env.JWT_SECRET) as JwtPayload;
+    
+    const userSession = await sessionService.findUserSessionByToken(token);
 
-        const session = await getRepository(Session).findOne({ token });
-        if(!session) return res.sendStatus(401);
-
-        res.locals.userId = session.userId;
-        return next();
-
-    } catch (err) {
-        console.log(err);
-        return res.sendStatus(500);
+    if(Number(userSession) !== userId) {
+      throw new UnauthorizedError();
     }
+
+    req.user = { id: userId };
+    next();
+  } catch (e) {
+    throw new UnauthorizedError();
+  }
 }
